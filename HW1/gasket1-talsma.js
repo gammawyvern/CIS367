209ccaa1d10a3a510c166e07325b3a0ccd933ce9
Keagen <keegenjt@gmail.com>
2024-01-21 21:30:42 -0500

    Altered generation based on questions

--- HW1/gasket1-talsma.js
+++ HW1/gasket1-talsma.js
@@ -5,6 +5,10 @@ var points;
 
 var NumPoints = 5000;
 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
@@ -19,9 +23,12 @@ window.onload = function init()
     // First, initialize the corners of our gasket with three points.
 
     var vertices = [
-        vec2( -1, -1 ),
-        vec2(  0,  1 ),
-        vec2(  1, -1 )
+      // vec2( -1, -1 ),
+      // vec2(  0,  1 ),
+      // vec2(  1, -1 )
+      vec2(-1,  1),
+      vec2( 1,  1),
+      vec2( 0, -1)
     ];
 
     // Specify a starting point p for our iterations
@@ -42,7 +49,7 @@ window.onload = function init()
     for ( var i = 0; points.length < NumPoints; ++i ) {
         var j = Math.floor(Math.random() * 3);
         p = add( points[i], vertices[j] );
-        p = scale( 0.5, p );
+        p = scale( getRandomArbitrary(0.5, 0.5), p );
         points.push( p );
     }
 
