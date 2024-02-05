3bb48a163df7cdd570c7caa7c2ea61229005ebcf
Keagen <keegenjt@gmail.com>
2024-02-05 13:11:36 -0500

    Changed to simple rotation

--- HW2/talsma-triangle-interact.js
+++ HW2/talsma-triangle-interact.js
@@ -1,24 +1,38 @@
 let gl;
 let points;
 
-let x = y = 0.0;
-let xLoc, yLoc;
+let translation = vec2(0.0, 0.0);
+let translation_location = vec2(0.0, 0.0);
+
+let run_speed = 0.005;
 
 let dirs = [null, null];
-let speed = 0.005;
+
+const Rotation = {
+  IDLE: "idle",
+  CLOCKWISE: "clockwise",
+  COUNTER_CLOCKWISE: "counter-clockwise",
+};
+
+let rotation_input = Rotation.IDLE; 
+let current_rotation = Math.PI / 2;
+let rotation_location = 0.0;
 
 window.onload = function init() {
 
   window.addEventListener("keydown", function(e) {
     if (e.key == "ArrowLeft") {
       dirs[0] = false;
+      rotation_input = Rotation.COUNTER_CLOCKWISE;
     } else if (e.key == "ArrowRight") {
       dirs[0] = true;
+      rotation_input = Rotation.CLOCKWISE;
     } else if (e.key == "ArrowUp") {
       dirs[1] = true;
     } else if (e.key == "ArrowDown") {
       dirs[1] = false;
     } else if (e.key == "Space") {
+      rotation_input = Rotation.IDLE; 
       dirs[0] = null;
       dirs[1] = null;
     }
@@ -41,8 +55,9 @@ window.onload = function init() {
   var program = initShaders(gl, 'vertex-shader', 'fragment-shader');
   gl.useProgram(program);
 
-  xLoc = gl.getUniformLocation(program, "x");
-  yLoc = gl.getUniformLocation(program, "y");
+  translation_location[0] = gl.getUniformLocation(program, "x");
+  translation_location[1] = gl.getUniformLocation(program, "y");
+  rotation_location = gl.getUniformLocation(program, "rotation")
 
   var bufferID = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, bufferID);
@@ -58,11 +73,14 @@ function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-  x += dirs[0] ? speed : -speed; 
-  y += dirs[1] ? speed : -speed;
+  // translation[0] += dirs[0] ? run_speed : -run_speed; 
+  // translation[1] += dirs[1] ? run_speed : -run_speed;
+  //
+  // gl.uniform1f(translation_location[0], translation[0]);
+  // gl.uniform1f(translation_location[1], translation[1]);
 
-  gl.uniform1f(xLoc, x);
-  gl.uniform1f(yLoc, y);
+  current_rotation += Math.PI / 200;
+  gl.uniform1f(rotation_location, current_rotation);
 
   window.requestAnimationFrame(render);
 }
