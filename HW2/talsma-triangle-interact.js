8830a16de27db14ce2009017b2d92c405928f902
Keagen <keegenjt@gmail.com>
2024-02-05 20:55:54 -0500

    Changed triangle vertex colors

--- HW2/talsma-triangle-interact.js
+++ HW2/talsma-triangle-interact.js
@@ -28,9 +28,9 @@ let vertices = [
 ];
 
 let colors = [
-  vec3(1.0, 0.0, 0.0),
+  vec3(0.0, 1.0, 0.0),
   vec3(0.0, 0.0, 1.0),
-  vec3(0.0, 0.0, 1.0)
+  vec3(0.0, 0.0, 1.0),
 ];
 
 window.onload = function init() {
