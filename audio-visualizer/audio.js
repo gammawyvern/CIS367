8c2977afc3ec4c4c1adff648ac44320b906b5c0d
Keagen <keegenjt@gmail.com>
2024-02-22 10:46:30 -0500

    Added visualization to audio player

--- audio-visualizer/audio.js
+++ audio-visualizer/audio.js
@@ -1,6 +1,13 @@
-window.onload = function() {
+function setupAudioVisualizer() {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const analyser = audioContext.createAnalyser();
+  analyser.fftSize = 256;
+
   const audioPlayer = document.getElementById('audioPlayer');
   const fileInput = document.getElementById('audioFileInput');
+  const source = audioContext.createMediaElementSource(audioPlayer);
+  source.connect(analyser);
+  analyser.connect(audioContext.destination);
 
   fileInput.addEventListener('change', function(event) {
     const file = event.target.files[0];
@@ -10,9 +17,49 @@ window.onload = function() {
 
       audioPlayer.play().then(() => {
         console.log('Audio playback started');
+        visualizeFrequencyData(analyser);
       }).catch(err => {
         console.error('Error starting audio playback:', err);
       });
     }
   });
+}
+
+function visualizeFrequencyData(analyser) {
+  const canvas = document.getElementById('visualizer');
+  const ctx = canvas.getContext('2d');
+
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  function draw() {
+    analyser.getByteFrequencyData(dataArray);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const barWidth = (canvas.width / bufferLength) * 2.5;
+    let barHeight;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+
+      ctx.fillStyle = 'rgb(0, 153, 242)';
+      (0.0, 0.6, 0.95, 1.0);
+      ctx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+
+      x += barWidth + 1;
+    }
+
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+}
+
+window.onload = function() {
+  setupAudioVisualizer();
 };
+
+
+
