766051889624e5cc79f50aea2abf55963948d315
Keagen <keegenjt@gmail.com>
2024-03-15 12:19:52 -0400

    Added textures to render on floor and boxes

--- hw3/talsma-three.js
+++ hw3/talsma-three.js
@@ -13,45 +13,64 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.setClearColor(0xFFFFFF);
 
-// Add meshes
-let floorWidth = 20;
-let floorHeight = 40;
-let floorGeometry = new THREE.PlaneGeometry(floorWidth, floorHeight);
-let floorMaterial = new THREE.MeshStandardMaterial({color: 0x009900});
-let floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
-floorMesh.position.set(0, -1, -3);
-floorMesh.rotation.set(-Math.PI/2, 0, 0);
-scene.add(floorMesh);
+// Load textures
+let floor_width = 20;
+let floor_height = 40;
+let box_width = 0.9;
+let box_height = 2;
+let box_depth = 0.2;
+let texture_loader = new THREE.TextureLoader(); 
 
-let boxWidth = 0.9;
-let boxHeight = 2;
-let boxDepth = 0.2;
-let boxGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-let boxMaterial = new THREE.MeshStandardMaterial({color: 0x00ffff});
+// Load in textures
+let grass = texture_loader.load("./assets/grass.png");
+let stone = texture_loader.load("./assets/stone_wall.png");
+
+grass.wrapS = THREE.RepeatWrapping;
+grass.wrapT = THREE.RepeatWrapping;
+grass.repeat.set(floor_width/2, box_height/2);
+
+stone.wrapS = THREE.RepeatWrapping;
+stone.wrapT = THREE.RepeatWrapping;
+stone.repeat.set(floor_width/2, box_height/2);
+
+// Add floor mesh
+let floor_geometry = new THREE.PlaneGeometry(floor_width, floor_height);
+let floor_material = new THREE.MeshStandardMaterial({
+  map: grass,
+  metalness: 0.25,
+  roughness: 0.25
+});
+let floor_mesh = new THREE.Mesh(floor_geometry, floor_material);
+floor_mesh.position.set(0, -1, -3);
+floor_mesh.rotation.set(-Math.PI/2, 0, 0);
+scene.add(floor_mesh);
+
+// Add boxe meshes to scene
+let box_geometry = new THREE.BoxGeometry(box_width, box_height, box_depth);
+let box_material = new THREE.MeshStandardMaterial({
+  map: stone,
+  metalness: 0,
+  roughness: 1
+});
 for (let i = 0; i < 14; i++) {
-  let boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-  boxMesh.position.set(i - 7, 1, -5);
-  boxMesh.receiveShadow = true;
-  boxMesh.castShadow = true;
-  scene.add(boxMesh);
-  boxes.push(boxMesh);
+  let box_mesh = new THREE.Mesh(box_geometry, box_material);
+  box_mesh.position.set(i - 7, 1, -5);
+  box_mesh.receiveShadow = true;
+  box_mesh.castShadow = true;
+  scene.add(box_mesh);
+  boxes.push(box_mesh);
 }
-// let boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-// boxMesh.position.set(-2, 1, -5);
-// scene.add(boxMesh);
 
-// Camera 
+// Camera / Lighting
 let ambient = new THREE.AmbientLight(0x888888);
 scene.add(ambient);
-let lightIntensity = 10;
-let pointLight = new THREE.PointLight(0xffffff, lightIntensity);
-pointLight.position.set(2, 4, -2);
-scene.add(pointLight);
+let light_intensity = 10;
+let point_light = new THREE.PointLight(0xffffff, light_intensity);
+point_light.position.set(2, 4, -2);
+scene.add(point_light);
 renderer.shadowMap.enabled = true;
-floorMesh.receiveShadow = true;
-// boxMesh.receiveShadow = true;
-// boxMesh.castShadow = true;
-pointLight.castShadow = true;
+floor_mesh.receiveShadow = true;
+point_light.castShadow = true;
 
 // Run animation loop
 function animate() {
