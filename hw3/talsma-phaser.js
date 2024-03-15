3c0b1143b149477b30a063de144dbe240de95e1a
Keagen <keegenjt@gmail.com>
2024-03-15 18:02:47 -0400

    Added simple phaser code to control player
    Note that there is a problem with setCollideWorldBounds() for some
    reason, so that needs to be fixed or implemented still.

--- hw3/talsma-phaser.js
+++ hw3/talsma-phaser.js
@@ -1,3 +1,6 @@
+let player;
+let cursors;
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -12,14 +15,29 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-
+  this.load.image("player", "./assets/player_sprite.png");
 }
 
 function create() {
+  player = this.add.sprite(400, 300, "player");
+  player.texture.setFilter(Phaser.Textures.FilterMode.NEAREST); 
+  player.setScale(4);
+  // player.setCollideWorldBounds(true);
 
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  if (cursors.left.isDown) {
+    player.x -= 4;
+  } else if (cursors.right.isDown) {
+    player.x += 4;
+  }
 
+  if (cursors.up.isDown) {
+    player.y -= 4;
+  } else if (cursors.down.isDown) {
+    player.y += 4;
+  }
 }
 
