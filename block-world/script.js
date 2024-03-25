90723284fc910dfd0a37356719a8040999e363ad
Keagen <keegenjt@gmail.com>
2024-03-25 12:49:18 -0400

    Added ambient lighting and moved camera
    Now you can tell it's an actualy 3d cube being rendered.

--- block-world/script.js
+++ block-world/script.js
@@ -2,15 +2,23 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.y = 5;
+// Messing with manual camera position for now
+camera.position.x = 2;
+camera.position.y = 2;
 camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xbbbbbb);
 document.body.appendChild(renderer.domElement);
+
+const ambient = new THREE.AmbientLight(0x888888);
+scene.add(ambient);
 
 const texture_loader = new THREE.TextureLoader(); 
 const dirt_texture = texture_loader.load("./assets/dirt.png");
+dirt_texture.magFilter = THREE.NearestFilter; 
+dirt_texture.minFilter = THREE.NearestFilter;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
