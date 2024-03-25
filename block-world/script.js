1ec5a2559e4c78a653c6f786a44f3685e79c4ea2
Keagen <keegenjt@gmail.com>
2024-03-25 16:12:14 -0400

    Added create_world() with simple floor for now

--- block-world/script.js
+++ block-world/script.js
@@ -3,10 +3,7 @@ import { FlyControls } from "addons/fly-controls";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// Messing with manual camera position for now
-camera.position.x = 2;
 camera.position.y = 2;
-camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -21,25 +18,46 @@ const dirt_texture = texture_loader.load("./assets/dirt.png");
 dirt_texture.magFilter = THREE.NearestFilter; 
 dirt_texture.minFilter = THREE.NearestFilter;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({
-  map: dirt_texture,
-  roughness: 0.2
-}); 
-const block = new THREE.Mesh(geometry, material);
-scene.add(block);
-
 const controls = new FlyControls(camera, renderer.domElement);
-controls.movementSpeed = 5;
 controls.domElement = renderer.domElement;
+controls.movementSpeed = 5;
+controls.rollEnabled = false;
 controls.rollSpeed = Math.PI / 3;
 controls.autoForward = false;
-controls.dragToLook = true;
+// TODO would like to just remove mouse controls totally 
+controls.dragToLook = true; 
 
+function create_world() {
+  let geometry = new THREE.BoxGeometry(1, 1, 1);
+  let material = new THREE.MeshStandardMaterial({
+    map: dirt_texture,
+    roughness: 0.2
+  }); 
+
+  let floor = new THREE.InstancedMesh(geometry, material, 10*10); 
+
+  const matrix = new THREE.Matrix4();
+  for (let x = 0; x < 10; x++) {
+    for (let z = 0; z < 10; z++) {
+      matrix.makeTranslation(x - 5, 0, z - 5); 
+      floor.setMatrixAt((x * 10) + z, matrix); 
+    }
+  }
+
+  scene.add(floor);
+}
+
+let prev_time = performance.now();
 function animate() {
+  let current_time = performance.now();
+  let delta = (current_time - prev_time) / 1000; 
+  prev_time = current_time;
+  console.log(delta);
+
   requestAnimationFrame(animate);
-  controls.update(0.01);
+  controls.update(delta);
   renderer.render(scene, camera);
 }
 
+create_world();
 animate();
