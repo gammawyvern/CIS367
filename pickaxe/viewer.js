38e024960597952bda005218638c802dbac26e53
Keagen <keegenjt@gmail.com>
2024-04-15 19:06:53 -0400

    Added sword and swap to sword

--- pickaxe/viewer.js
+++ pickaxe/viewer.js
@@ -6,17 +6,52 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x6e5342);
 document.body.appendChild(renderer.domElement);
 
-// Load and display the 3D model
+// Allow switching between models
+let sword_model;
+let pickaxe_model;
+let on_pickaxe = true;
+
 const loader = new THREE.GLTFLoader();
 loader.load("./pickaxe.glb", (gltf) => {
-  gltf.scene.position.y -= 0.5
-  scene.add(gltf.scene);
+  gltf.scene.position.y -= 0.5;
+  pickaxe_model = gltf.scene;
+  scene.add(pickaxe_model);
 });
 
-// camera.position.y = 0.5;
+loader.load("./sword.glb", (gltf) => {
+  sword_model = gltf.scene;
+});
+
+
+// Add quick swapping
+function toggle_model() {
+  if (on_pickaxe) {
+    scene.remove(pickaxe_model)
+    scene.add(sword_model)
+  } else {
+    scene.remove(sword_model)
+    scene.add(pickaxe_model)
+  }
+
+  on_pickaxe = !on_pickaxe;
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        toggle_model();
+    }
+});
+
+// Setup to be able to view models
 camera.position.z = 3;
 let ambient = new THREE.AmbientLight(0xffffff);
 scene.add(ambient);
+let light_intensity = 2;
+let point_light = new THREE.PointLight(0xffffff, light_intensity);
+point_light.position.set(0, 5, 1);
+scene.add(point_light);
+renderer.shadowMap.enabled = true;
+point_light.castShadow = true;
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; 
